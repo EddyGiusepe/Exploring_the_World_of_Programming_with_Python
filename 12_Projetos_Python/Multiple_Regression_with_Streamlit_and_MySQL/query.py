b67876f4a74ae6e7848e5763688601173ae5b1be
EddyGiusepe <eddychirinos.unac@gmail.com>
2024-05-02 23:45:11 -0300

    Conectando com MySQL e através de um container

---
+++ 12_Projetos_Python/Multiple_Regression_with_Streamlit_and_MySQL/query.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import mysql.connector
+#pip install mysql-connector-python
+import streamlit as st
+
+conn = mysql.connector.connect(
+host="localhost",
+port="3306",
+user="root",
+passwd="123456",
+db="multiple_regression")
+
+c = conn.cursor()
+
+
+def view_all_data():
+	c.execute('SELECT * FROM economic_index')
+	data = c.fetchall()
+	return data
+
