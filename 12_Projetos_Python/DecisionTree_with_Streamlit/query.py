50911d6c0f1ff9dee896007b105a70bf5fe8b955
EddyGiusepe <eddychirinos.unac@gmail.com>
2024-05-03 23:31:24 -0300

    Script para exportar Dados do DB MySQL

---
+++ 12_Projetos_Python/DecisionTree_with_Streamlit/query.py
@@ -0,0 +1,18 @@
+#import mysql.connector
+#pip install mysql-connector-python
+#import streamlit as st
+
+#conn = mysql.connector.connect(
+#host="localhost",
+#port="3306",
+#user="root",
+#passwd="",
+#db="streamlit")
+
+#c = conn.cursor()
+
+
+#def view_all_data():
+	#c.execute('SELECT Location,State,Region,Investment,Construction,BusinessType FROM insurance')
+	#data = c.fetchall()
+	#return data
