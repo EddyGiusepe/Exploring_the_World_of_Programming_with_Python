9344b4dae9a45cf9bc2ded1ada1dfc2270dea4c7
EddyGiusepe <eddychirinos.unac@gmail.com>
2024-05-26 21:41:28 -0300

    shell para Instalar Ollama

---
+++ 18_Ollama/Installing_and_Running_Ollama.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Baixa e executa o script de instalação do Ollama
+curl -fsSL https://ollama.com/install.sh | sh
+
+# Inicia o servidor Ollama
+ollama serve
+
+# Lembrar a execução ---> ./Installing_and_Running_Ollama.sh
+# Dar permissão --> chmod +x Installing_and_Running_Ollama.sh
+# ollama pull phi3:3.8b
+# ollama pull llama2:7b
