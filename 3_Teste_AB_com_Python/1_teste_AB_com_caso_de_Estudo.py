07e1895a8019bc1d1a236ae9ca1027f93d2c5967
EddyGiusepe <eddychirinos.unac@gmail.com>
2024-07-28 21:30:28 -0300

    Teste A/B

--- 3_Teste_AB_com_Python/1_teste_AB_com_caso_de_Estudo.py
+++ 3_Teste_AB_com_Python/1_teste_AB_com_caso_de_Estudo.py
@@ -11,3 +11,32 @@ Outro link de estudo:
 
 * https://morioh.com/a/b74819a8ce05/how-to-install-tensorflow-object-detect-api
 """
+
+"""
+Testes AB:
+* permite aos cientistas de dados medir a eficácia das mudanças de seu produto ou serviço, o que é importante, porque permite aos cientistas de dados tomar decisões baseadas em dados e não em intuições
+* ajuda a cientistas de dados a identificar o que há de mais mudanças efetivas em um produto ou serviço que é realmente importante
+* Testes AB de vendas nos ajuda a validar certas hipóteses sobre o que mudanças irão melhorar um produto ou um serviço 
+* Testes AB é uma prática em muitas indústrias (Marketing, )
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
