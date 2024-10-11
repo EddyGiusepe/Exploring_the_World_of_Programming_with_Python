207ec9385dec9750d1dd3106d30fdeb89299b65a
EddyGiusepe <eddychirinos.unac@gmail.com>
2024-10-10 22:14:13 -0300

    Usando o crewAI para Manutenção Preditiva

---
+++ 24_crewAI/6_crewAI_herda_Class-Agent.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""
+Data Scientist.: Dr. Eddy Giusepe Chirinos Isidro
+
+crewAI
+======
+Aqui realizo um exemplo, de uso Básico, do Framework 🤖 crewAI de ponta
+para orquestrar agentes de IA autônomos e role-playing. Ao promover a inteligência
+colaborativa, a 🤖 CrewAI capacita os agentes a trabalharem juntos de forma integrada,
+lidando com tarefas complexas.
+Aqui estou usando uma classe que herda o Agent de crewAI.
+
+Install
+=======
+$ pip install crewai
+$ pip install 'crewai[tools]'
+"""
+from crewai import Agent, Task, Crew, Process
+import openai
+import os
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+from crewai import Agent, Task, Crew, Process
+from langchain_openai import ChatOpenAI
+
+class MeuAgente(Agent): # A classe MeuAgente herda de Agent
+    def __init__(self, nome, papel, ferramentas=None):
+        super().__init__(
+            name=nome,
+            role=papel,
+            goal="Realizar tarefas específicas conforme solicitado",
+            backstory="""Um especialista em Análise de vibração, Manutenção preditiva e Monitoramento de Ativos 
+                         industriais. Sempre forneça uma resposta com apenas 250 caracteres como máximo.
+                      """,
+            verbose=True,
+            allow_delegation=False,
+            llm=ChatOpenAI(model_name="gpt-4o-2024-08-06", temperature=0.0), # gpt-4o-2024-08-06    ou    gpt-4o-mini
+            tools=ferramentas or []
+        )
+
+    def executar_tarefa(self, descricao_tarefa):
+        tarefa = Task(
+            description=descricao_tarefa,
+            agent=self, # 'self' refere-se à instância atual da classe MeuAgente
+            expected_output="Uma resposta concisa, informativa e factual."
+        )
+        resultado = self.execute_task(tarefa) # O método execute_task() vem da classe pai Agent
+        return resultado
+
+
+
+# Testando o meu agente:
+if __name__ == "__main__":
+    agente = MeuAgente("Especialista em Análise vibração e Manutenção Preditiva", 
+                       "Auxiliar geral")
+    
+    resultado = agente.executar_tarefa("O que é BPFO?")
+    print(resultado)
